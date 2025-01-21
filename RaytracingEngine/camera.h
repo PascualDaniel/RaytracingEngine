93067dc9e5522f376b9aa030d29997805142741c
Daniel Pascual <danielpascuallopez@hotmail.com>
2025-01-21 18:35:09 +0100

    Color Gammut correction, better refflection using Lambertian distribution.

--- RaytracingEngine/camera.h
+++ RaytracingEngine/camera.h
@@ -90,9 +90,10 @@ private:
             return color(0,0,0);
         hit_record rec;
 
-        if (world.hit(r, interval(0, infinity), rec)) {
-            vec3 direction = random_on_hemisphere(rec.normal);
-            return 0.5 * ray_color(ray(rec.p, direction), depth-1, world);
+        if (world.hit(r, interval(0.001, infinity), rec)) {
+            vec3 direction = rec.normal + random_unit_vector();
+            return 0.1 * ray_color(ray(rec.p, direction), depth-1, world);
+           // return 0.5 * ray_color(ray(rec.p, direction), depth-1, world);
         }
 
 

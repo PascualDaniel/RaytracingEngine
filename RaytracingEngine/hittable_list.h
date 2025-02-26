03c8ff66915e7529aa78d5acdcb0080b903e0c5a
Daniel Pascual <danielpascuallopez@hotmail.com>
2025-02-26 20:44:52 +0100

    Optimization of Bounding Volumes

--- RaytracingEngine/hittable_list.h
+++ RaytracingEngine/hittable_list.h
@@ -1,6 +1,7 @@
 #ifndef HITTABLE_LIST_H
 #define HITTABLE_LIST_H
 
+#include "aabb.h"
 #include "hittable.h"
 
 
@@ -19,6 +20,7 @@ public:
 
     void add(shared_ptr<hittable> object) {
         objects.push_back(object);
+        bbox = aabb(bbox, object->bounding_box());
     }
 
     bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
@@ -36,6 +38,10 @@ public:
 
         return hit_anything;
     }
+    aabb bounding_box() const override { return bbox; }
+
+private:
+    aabb bbox;
 };
 
 #endif
--- RaytracingEngine/rtweekend.h
+++ RaytracingEngine/rtweekend.h
@@ -29,11 +29,16 @@ inline double random_double() {
     static std::mt19937 generator;
     return distribution(generator);
 }
+
 inline double random_double(double min, double max) {
     // Returns a random real in [min,max).
     return min + (max - min) * random_double();
 }
 
+inline int random_int(int min, int max) {
+    // Returns a random integer in [min,max].
+    return int(random_double(min, max+1));
+}
 // Common Headers
 
 #include "color.h"

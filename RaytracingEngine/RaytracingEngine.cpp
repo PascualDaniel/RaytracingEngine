a13c3438dd55acc7a93c867e321c3ab196d7402d
Daniel Pascual López <uo269728@uniovi.es>
2025-03-22 22:36:42 +0100

    Simple emisive lights

--- RaytracingEngine/RaytracingEngine.cpp
+++ RaytracingEngine/RaytracingEngine.cpp
@@ -66,7 +66,7 @@ void bouncing_spheres() {
     cam.image_width       = 400;
     cam.samples_per_pixel = 100;
     cam.max_depth         = 50;
-
+    cam.background        = color(0.70, 0.80, 1.00);
     cam.vfov     = 20;
     cam.lookfrom = point3(13,2,3);
     cam.lookat   = point3(0,0,0);
@@ -91,7 +91,8 @@ void checkered_spheres() {
     cam.image_width       = 400;
     cam.samples_per_pixel = 100;
     cam.max_depth         = 50;
-
+    cam.background        = color(0.70, 0.80, 1.00);
+    
     cam.vfov     = 20;
     cam.lookfrom = point3(13,2,3);
     cam.lookat   = point3(0,0,0);
@@ -113,7 +114,8 @@ void roshar() {
     cam.image_width       = 400;
     cam.samples_per_pixel = 100;
     cam.max_depth         = 50;
-
+    cam.background        = color(0.70, 0.80, 1.00);
+    
     cam.vfov     = 20;
     cam.lookfrom = point3(0,0,12);
     cam.lookat   = point3(0,0,0);
@@ -136,7 +138,9 @@ void perlin_spheres() {
     cam.image_width       = 400;
     cam.samples_per_pixel = 100;
     cam.max_depth         = 50;
+    cam.background        = color(0.70, 0.80, 1.00);
 
+    
     cam.vfov     = 20;
     cam.lookfrom = point3(13,2,3);
     cam.lookat   = point3(0,0,0);
@@ -169,7 +173,9 @@ void quads() {
     cam.image_width       = 400;
     cam.samples_per_pixel = 100;
     cam.max_depth         = 50;
+    cam.background        = color(0.70, 0.80, 1.00);
 
+    
     cam.vfov     = 80;
     cam.lookfrom = point3(0,0,9);
     cam.lookat   = point3(0,0,0);
@@ -179,12 +185,41 @@ void quads() {
 
     cam.render(world);
 }
+
+void simple_light() {
+    hittable_list world;
+
+    auto pertext = make_shared<noise_texture>(4);
+    world.add(make_shared<sphere>(point3(0,-1000,0), 1000, make_shared<lambertian>(pertext)));
+    world.add(make_shared<sphere>(point3(0,2,0), 2, make_shared<lambertian>(pertext)));
+
+    auto difflight = make_shared<diffuse_light>(color(4,4,4));
+    world.add(make_shared<quad>(point3(3,1,-2), vec3(2,0,0), vec3(0,2,0), difflight));
+
+    camera cam;
+
+    cam.aspect_ratio      = 16.0 / 9.0;
+    cam.image_width       = 400;
+    cam.samples_per_pixel = 100;
+    cam.max_depth         = 50;
+    cam.background        = color(0,0,0);
+
+    cam.vfov     = 20;
+    cam.lookfrom = point3(26,3,6);
+    cam.lookat   = point3(0,2,0);
+    cam.vup      = vec3(0,1,0);
+
+    cam.defocus_angle = 0;
+
+    cam.render(world);
+}
 int main() {
-    switch (5) {
+    switch (6) {
     case 1:  bouncing_spheres();  break;
     case 2:  checkered_spheres(); break;
     case 3:  roshar();             break;
     case 4:  perlin_spheres();     break;
     case 5:  quads();              break;
+    case 6:  simple_light();       break;
     }
 }

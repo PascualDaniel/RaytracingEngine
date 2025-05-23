dc41e0164067ffd1631cfe99d0be9f5d61d30df9
Daniel Pascual <danielpascuallopez@hotmail.com>
2025-05-23 20:26:59 +0200

    Cuda implementation start

--- RaytracingEngine/RaytracingEngine.cu
+++ RaytracingEngine/RaytracingEngine.cu
@@ -1,3 +1,6 @@
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
 #include "rtweekend.h"
 
 
@@ -13,6 +16,16 @@
 
 #define _CRT_SECURE_NO_WARNINGS
 
+#define checkCudaErrors(val) check_cuda( (val), #val, __FILE__, __LINE__ )
+void check_cuda(cudaError_t result, char const* const func, const char* const file, int const line) {
+    if (result) {
+        std::cerr << "CUDA error = " << static_cast<unsigned int>(result) << " at " <<
+            file << ":" << line << " '" << func << "' \n";
+        // Make sure we call CUDA Device Reset before exiting
+        cudaDeviceReset();
+        exit(99);
+    }
+}
 
 void bouncing_spheres() {
     hittable_list world;
